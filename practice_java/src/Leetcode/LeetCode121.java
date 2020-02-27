f49b396aa915c7bab6574947b8a7562ab888e69c
Faizan Jamil <faizan34@gmail.com>
2020-02-27 12:54:24 -0500

    started leetcode problem 121

---
+++ practice_java/src/Leetcode/LeetCode121.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+class LeetCode121 {
+    public static int maxProfit(int[] prices) {
+        // find smallest element and its index
+        // find largest element after smallest and its index
+       
+        int[] sorted = copyArray(prices);
+        Arrays.sort(sorted);
+        int largest = sorted[sorted.length - 1];
+        int smallest = sorted[0];
+        int smallestIndex = 0;
+        int largestIndex = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] == largest) {
+                largestIndex = i;
+            }
+            if (prices[i] == smallest && i > largestIndex) {
+                smallestIndex = i;
+            }
+        }
+
+        
+        return smallestIndex;
+    }
+    public static void main(String[] args) {
+        int[] testCase = {7,1,5,3,6,4};
+        int profit = maxProfit(testCase);
+    }
+    public static int[] copyArray(int[] array) {
+        int[] copy = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            copy[i] = array[i];
+        }
+        return copy;
+    }
+}
